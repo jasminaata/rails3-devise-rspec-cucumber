556b8a1b333be0ef48eb2fe9abd31de08b8128f6
Jasmina Atanassova <jasminaata@gmail.com>
2012-09-10 16:39:07 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3DeviseRspecCucumber::Application.config.secret_token = '26b21a32e0b30bde2a412abad3ef8c3091e85068ec7f1bf99ad6542d22a89ecaf53aba779e45cf8db4785510f989948b8f80c6f20498ee81661087668517cfe3'
